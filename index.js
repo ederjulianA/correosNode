7944469d71b0f7c22f77224ba15c909fb1f15e2d
ivan jose rojas cabrales <ivan.rojas.sis@gmail.com>
2022-01-18 10:16:19 -0500

    We added the espress limit to 50 mb in order to allow big files in the body

--- index.js
+++ index.js
@@ -4,10 +4,11 @@ const nodemailer = require('nodemailer');
 const app = express();
 app.use(
     express.urlencoded({
-      extended: true
+      extended: true,
+      limit: '50mb'
     })
   )
-  app.use(express.json());
+  app.use(express.json({limit: '50mb'}));
 //const port = 3000;
 
 var respuesta;
